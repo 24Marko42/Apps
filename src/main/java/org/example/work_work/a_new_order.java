782fe4b3da7719214e408488c1c6221402447981
marko <markoffka.kuznetsov@mail.ru>
2024-11-24 23:37:01 +0700

    second app

--- src/main/java/org/example/work_work/a_new_order.java
+++ src/main/java/org/example/work_work/a_new_order.java
@@ -5,28 +5,29 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.converter.IntegerStringConverter;
 
 public class a_new_order extends Application {
     @Override
     public void start(Stage primaryStage) {
         // Создаем чекбоксы для выбора блюд
-        CheckBox pizza = new CheckBox("Пицца - 500 руб.");
-        CheckBox sushi = new CheckBox("Суши - 300 руб.");
-        CheckBox burger = new CheckBox("Бургер - 200 руб.");
+        CheckBox pivo = new CheckBox("Пиво - 500 руб.");
+        CheckBox viski = new CheckBox("Виски - 300 руб.");
+        CheckBox soju = new CheckBox("Соджа - 200 руб.");
 
         // Поля для ввода количества порций
-        TextField pizzaQuantity = new TextField("1");
-        TextField sushiQuantity = new TextField("1");
-        TextField burgerQuantity = new TextField("1");
+        TextField pivoQuantity = new TextField("1");
+        TextField viskiQuantity = new TextField("1");
+        TextField sojuQuantity = new TextField("1");
 
         // Делаем поля количества активными только при выборе блюда
-        pizzaQuantity.setDisable(true);
-        sushiQuantity.setDisable(true);
-        burgerQuantity.setDisable(true);
+        pivoQuantity.setDisable(true);
+        viskiQuantity.setDisable(true);
+        sojuQuantity.setDisable(true);
 
-        pizza.setOnAction(event -> pizzaQuantity.setDisable(!pizza.isSelected()));
-        sushi.setOnAction(event -> sushiQuantity.setDisable(!sushi.isSelected()));
-        burger.setOnAction(event -> burgerQuantity.setDisable(!burger.isSelected()));
+        pivo.setOnAction(event -> pivoQuantity.setDisable(!pivo.isSelected()));
+        viski.setOnAction(event -> viskiQuantity.setDisable(!viski.isSelected()));
+        soju.setOnAction(event -> sojuQuantity.setDisable(!soju.isSelected()));
 
         // Текстовое поле для отображения чека
         TextArea receipt = new TextArea();
@@ -35,40 +36,55 @@ public class a_new_order extends Application {
         // Кнопка для расчета стоимости
         Button calculate = new Button("Посчитать");
 
+        pivoQuantity.setTextFormatter(new TextFormatter<>(new IntegerStringConverter(), 1, change -> {
+            String newText = change.getControlNewText();
+            return newText.matches("\\d*") ? change : null; //Метод matches проверяет, соответствует ли строка newText регулярному выражению \\d*
+        }));
+        viskiQuantity.setTextFormatter(new TextFormatter<>(new IntegerStringConverter(), 1, change -> {
+            String newText = change.getControlNewText();
+            return newText.matches("\\d*") ? change : null; //Метод matches проверяет, соответствует ли строка newText регулярному выражению \\d*
+        }));
+        sojuQuantity.setTextFormatter(new TextFormatter<>(new IntegerStringConverter(), 1, change -> {
+            String newText = change.getControlNewText();
+            return newText.matches("\\d*") ? change : null; //Метод matches проверяет, соответствует ли строка newText регулярному выражению \\d*
+        }));
+
         calculate.setOnAction(event -> {
             double total = 0;
             StringBuilder receiptText = new StringBuilder("Ваш заказ:\n");
 
-            if (pizza.isSelected()) {
-                int qty = Integer.parseInt(pizzaQuantity.getText());
-                double cost = 500 * qty;
+            if (pivo.isSelected()) {
+                int skilki = Integer.parseInt(pivoQuantity.getText());
+                double cost = 500 * skilki;
                 total += cost;
-                receiptText.append("Пицца: ").append(qty).append(" шт. = ").append(cost).append(" руб.\n");
+                receiptText.append("pivo: ").append(skilki).append(" шт. = ").append(cost).append(" руб.\n");
             }
-            if (sushi.isSelected()) {
-                int qty = Integer.parseInt(sushiQuantity.getText());
-                double cost = 300 * qty;
+            if (viski.isSelected()) {
+                int skilki = Integer.parseInt(viskiQuantity.getText());
+                double cost = 300 * skilki;
                 total += cost;
-                receiptText.append("Суши: ").append(qty).append(" шт. = ").append(cost).append(" руб.\n");
+                receiptText.append("viski: ").append(skilki).append(" шт. = ").append(cost).append(" руб.\n");
             }
-            if (burger.isSelected()) {
-                int qty = Integer.parseInt(burgerQuantity.getText());
-                double cost = 200 * qty;
+            if (soju.isSelected()) {
+                int skilki = Integer.parseInt(sojuQuantity.getText());
+                double cost = 200 * skilki;
                 total += cost;
-                receiptText.append("Бургер: ").append(qty).append(" шт. = ").append(cost).append(" руб.\n");
+                receiptText.append("soju: ").append(skilki).append(" шт. = ").append(cost).append(" руб.\n");
             }
 
-            receiptText.append("Итого: ").append(total).append(" руб.");
+            receiptText.append("сколько ты отдашь чтоб умереть от передозировки алкоголя: ").append(total).append(" руб.");
             receipt.setText(receiptText.toString());
         });
 
         // Размещение элементов в вертикальном контейнере
-        VBox layout = new VBox(10, pizza, pizzaQuantity, sushi, sushiQuantity, burger, burgerQuantity, calculate, receipt);
+        VBox layout = new VBox(10, pivo, pivoQuantity, soju, sojuQuantity, viski, viskiQuantity, calculate, receipt);
 
         // Настройка сцены и окна
-        Scene scene = new Scene(layout, 400, 400);
+        Scene scene = new Scene(layout);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Заказ в ресторане");
+        primaryStage.setMinWidth(500); // Устанавливает минимальную ширину окна
+        primaryStage.setMinHeight(500); // Устанавливает минимальную высоту окна
         primaryStage.show();
     }
 
@@ -76,3 +92,7 @@ public class a_new_order extends Application {
         launch(args);
     }
 }
+
+/*идея: придумать приложение, которое будет брать твою геолокацию, определять магазины с алкоголем в радиусе
+* 500 метров и предоставлять ассортимент алкогольной продукции, тип где можно дешевле купить(use API магазинов(?) ).
+* Потом оно будет прокладывать маршрут, тип если нужно закупиться для компании людей, а вкусы у всех разные (пример: я люблю соджу, а она только в кб)*/
